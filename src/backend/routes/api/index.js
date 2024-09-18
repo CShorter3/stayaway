9d33f33951e0c21d3e8258b69e79fd225e2a1db1
CShorter3 <CalidShorter3@gmail.com>
2024-09-18 18:34:56 -0400

    Started phase 4; end at user login API route

--- src/backend/routes/api/index.js
+++ src/backend/routes/api/index.js
@@ -1,8 +1,23 @@
 const router = require('express').Router();
-const { restoreUser } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
+//const { restoreUser } = require('../../utils/auth.js'); for testing 
+//const { User } = require('../../db/models');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
-router.use(restoreUser);
+// Connect restoreUser middleware to the API router
+  // If current user session is valid, set req.user to the user in the database
+  // If current user session is not valid, set req.user to null
+  router.use(restoreUser);
+
+  router.use('/session', sessionRouter);
+  
+  router.use('/users', usersRouter);
+  
+  router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+  });
+  
+  module.exports = router;
 
 /* VARIOUS TESTING ROUTES
 //const { setTokenCookie, requireAuth } = require('../../utils/auth.js');
