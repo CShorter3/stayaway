57cda37b44aab3367ce0619b5d25b50117fccb84
thaag7734 <thaag7734@gmail.com>
2024-09-19 17:48:54 -0400

    Add user signup feature

--- src/backend/routes/api/users.js
+++ src/backend/routes/api/users.js
@@ -1,4 +1,31 @@
-const express = require('express')
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const { setTokenCookie, requireAuth } = require('../../utils/auth')
+const { User } = require('../../db/models')
+
 const router = express.Router();
+
+/**** SIGNUP ****/
+router.post('/', async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const hashedPassword = bcrypt.hashSync(password);
+
+  const user = await User.create({
+    email,
+    username,
+    hashedPassword,
+  });
+
+  const safeUser = {
+    id: user.id,
+    email: user.email,
+    username: user.username,
+  };
+
+  return res.json({
+    user: safeUser,
+  });
+})
 
 module.exports = router;
