e4d31ffd0c710fdfd109fa3620ed3f9b8462512a
thaag7734 <thaag7734@gmail.com>
2024-09-23 16:24:52 -0400

    Set allowNull to false on Spot model's ownerId

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -18,6 +18,7 @@ module.exports = (sequelize, DataTypes) => {
   Spot.init({
     ownerId: {
       type: DataTypes.INTEGER,
+      allowNull: false,
     },
     address: {
       type: DataTypes.STRING,
