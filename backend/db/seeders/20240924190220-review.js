746ac566a78637afec37c215415d8eeed7d8339b
thaag7734 <thaag7734@gmail.com>
2024-09-24 15:10:32 -0400

    Add seeder for Reviews

---
+++ backend/db/seeders/20240924190220-review.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const { Review } = require('../models');
+
+const options = { schema: process.env.SCHEMA };
+
+const seedData = [
+  {
+    spotId: 2,
+    userId: 1,
+    review: 'Lorem ipsum odor amet, consectetuer adipiscing elit. Nec egestas cras taciti aenean ligula ex turpis amet vulputate sem sollicitudin scelerisque justo pretium nibh scelerisque fermentum lectus arcu.',
+    stars: 4
+  }
+];
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Reviews', seedData, options);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    for (const review of seedData) {
+      await Review.destroy({ where: review });
+    }
+  }
+};
