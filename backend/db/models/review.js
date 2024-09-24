568944d407b5e785fdc01fdb1d0535cc0d593e10
thaag7734 <thaag7734@gmail.com>
2024-09-24 16:29:21 -0400

    Change type of review column in Review model/migration from INTEGER -> STRING

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -23,7 +23,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
     },   
     review: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.STRING,
     },    
     stars: {
       type: DataTypes.INTEGER,
