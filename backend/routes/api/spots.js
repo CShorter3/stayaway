7c1139c7281b9217af5440f3af2591f732e80631
CShorter3 <CalidShorter3@gmail.com>
2024-09-24 17:01:25 -0400

    Add route to retrieve basic spot details of spot on id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,18 +1,18 @@
 const router = require('express').Router();  // import server and route handling functionality
-const { Spot, /*User*/ } = require('../../db/models'); // import user model
+const { Spot, Review, SpotImage, User, Sequelize } = require('../../db/models'); // import user model
 
 // restoreUser verifies token on request
 // requireAuth directs an (un)authorized request
-const { restoreUser, requireAuth } = require('../utils'); 
+const { restoreUser, requireAuth } = require('../../utils/auth'); 
 
-/**** 1. Get all spots ****/
+/**** Get all spots ****/
 router.get('/',
     async (req, res) => {
     const allSpots = await Spot.findAll();
-      return res.json(allSpots);
+      return res.json({allSpots});
     });
 
-/**** 2. Get all spots of current user ****/
+/**** Get all spots of current user ****/
 router.get('/current', 
   restoreUser, requireAuth,
   async(req, res, next) => {
@@ -31,5 +31,47 @@ router.get('/current',
     next(error); 
   }
 });
+
+/*** Get spot details on id */
+
+router.get('/:spotId',
+  async(req, res, next) => {
   
+  const { spotId } = req.params;
+
+  try {
+    // Capture spot deatils including associated images and owner details. 
+    const spot = await Spot.findOne({
+      where: { id: spotId },
+    });
+
+    if(!spot){
+      return res.status(404).json({
+        message: "Spot couldn't be found"
+      });
+    }
+
+    const detailedResponse = {
+      id: spot.id,
+      ownerId: spot.address,
+      city: spot.city,
+      state: spot.state,
+      country: spot.country,
+      lat: spot.lat,
+      lng: spot.lng,
+      name: spot.name,
+      description: spot.description,
+      price: spot.price,
+      createdAt: spot.createdAt,
+      updatedAt: spot.updatedAt,
+    };
+
+    return res.status(200).json(detailedResponse);
+
+  } catch (error) {
+      next(error);
+  }
+
+});
+
 module.exports = router;
