170e6fddf0d4a4f487c1321fec49004a7965f009
CShorter3 <CalidShorter3@gmail.com>
2024-09-25 10:39:44 -0400

    move owner alias from wrong model to proper user model on hasMany(models.Spot) association

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.hasMany(models.Spot, { foreignKey: 'ownerId' });
+      this.hasMany(models.Spot, { as: 'Owner', foreignKey: 'ownerId' });
       this.hasMany(models.Review, { foreignKey: 'userId' });
       this.hasMany(models.Booking, { foreignKey: 'userId' });
     }
