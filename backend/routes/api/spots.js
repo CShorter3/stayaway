2ce388c99498f409b7b292c612fcacea89b66ac8
CShorter3 <CalidShorter3@gmail.com>
2024-09-25 20:18:48 -0400

    drop spot-post branch and spots-put-delete branch to properly checkout next feature branch from dev

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const { Spot } = require('../../db/models');
 //const spot = require('../../db/models/spot');
 
-
 router.get('/',
     async (req, res) => {
     const allSpots = await Spot.findAll();
