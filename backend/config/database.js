072fb1a41eee3d4594538b5899958f811bcbfaca
CShorter3 <CalidShorter3@gmail.com>
2024-10-15 12:27:33 -0400

    test migrations in development enviroment

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,17 +1,17 @@
 const config = require('./index');
 
-//console.log('URL =====>', process.env.DATABASE_URL);
+console.log('URL =====>', process.env.DATABASE_URL);
 
 module.exports = {
-  /*development: {
+  development: {
     storage: config.dbFile,
     dialect: "sqlite",
     seederStorage: "sequelize",
     logQueryParameters: true,
     typeValidation: true,
     logging: console.log,
-  },*/
-  development: {
+  },
+  /*development: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
@@ -25,7 +25,7 @@ module.exports = {
       schema: process.env.SCHEMA
     },
     logging: console.log,
-  },
+  },*/
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
