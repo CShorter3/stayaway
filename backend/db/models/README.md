b1c0ad65ec66bc71f3a2bbd7e4512fb91d3692ab
CShorter3 <CalidShorter3@gmail.com>
2024-10-21 07:30:40 -0400

    Adds backend login flow to README

--- backend/db/models/README.md
+++ backend/db/models/README.md
@@ -1,3 +1,5 @@
 bcrypt library deals with hashing passwords
 
 op library provides symbols that map to sql operators, enabling complex querying
+
+each model implements model scoping to help protect users info
--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -58,8 +58,13 @@ module.exports = (sequelize, DataTypes) => {
   {
     sequelize,
     modelName: 'User',
+    defaultScope: {
+      attributes: {
+        exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt'],
+      },
+    },
   });
-  
+
   return User;
 };
 
