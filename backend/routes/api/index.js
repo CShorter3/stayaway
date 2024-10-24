5911646335f3aa9bbf6db11f06b7f995519a9ec1
CShorter3 <CalidShorter3@gmail.com>
2024-10-23 20:59:16 -0400

    tested restoreUser and requireAuth

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,50 +1,45 @@
 const router = require('express').Router();
 const { restoreUser } = require('../../utils/auth.js'); // for testing 
-const { User } = require('../../db/models');
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 
 // Connect restoreUser middleware to the API router
-  // If current user session is valid, set req.user to the user in the database
-  // If current user session is not valid, set req.user to null
-  router.use(restoreUser);
+// If current user session is valid, set req.user to the user in the database
+// If current user session is not valid, set req.user to null
+router.use(restoreUser);
 
-  router.use('/session', sessionRouter);
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.use('/reviews', require('./reviews.js'));
+
+router.use('/review-images', require('./review-images.js'));
+
+router.use('/bookings', require('./bookings.js'));
+
+router.use('/spots', require('./spots.js'));
+
+router.use('/spot-images', require('./spot-images.js'));
   
-  router.use('/users', usersRouter);
+const { User } = require('../../db/models');
+const { setTokenCookie, requireAuth } = require('../../utils/auth.js');
 
-  router.use('/reviews', require('./reviews.js'));
-
-  router.use('/review-images', require('./review-images.js'));
-
-  router.use('/bookings', require('./bookings.js'));
+router.post('/test', function(req, res) {
+  console.log('hit test route') 
+  console.log(req.body)
+  res.json({ requestBody: req.body });
+});
   
-  router.use('/spots', require('./spots.js'));
-
-  router.use('/spot-images', require('./spot-images.js'));
-  
-  // router.post('/test', (req, res) => {
-  //   console.log("hit test route!")
-  //   console.log(req.body)
-  //   res.json({ requestBody: req.body });
-  // });
-  
-  const { setTokenCookie, requireAuth } = require('../../utils/auth.js');
-  router.post('/test', function(req, res) {
-    console.log('hit test route') 
-    console.log(req.body)
-    res.json({ requestBody: req.body });
-    });
-    
-  router.get('/set-token-cookie', async (_req, res) => {
-    const user = await User.findOne({
-      where: {
-        username: 'Demo-lition'
-        }
-        });
-        setTokenCookie(res, user);
-        return res.json({ user: user });
-        });
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: 'Demo-lition'
+    }
+  });
+  setTokenCookie(res, user);
+  return res.json({ user: user });
+});
           
 router.get(
   '/restore-user',
@@ -60,7 +55,6 @@ router.get(
     return res.json(req.user);
   }
 );
-
 
 
 module.exports = router;
