cd29bac079a59a47f357f6a460c24d306e0d38df
CShorter3 <CalidShorter3@gmail.com>
2024-10-24 22:19:36 -0400

    Adds Redux store enhancer. Applies thunk, logger, and compose in relevant envs

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -8,4 +8,21 @@ const rootReducer = combineReducers({
 
 });
 
-export default store;
+let enhancer;
+// prepare enhancer to apply relevant middlewares to the store's dispath method
+if(import.meta.env.MODE === 'production'){
+    // apply thunk in production envioment
+    enhancer = applyMiddleware(thunk);
+} else {
+    // apply thunk, compose, and logger in development environment
+    const logger = (await import("redux-logger")).default;
+    const composeEnhancers = 
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+
+const configureStore = (preloadedState) => {
+    return createStore(rootReducer, preloadedState, enhancer)
+}
+
+export default {store, configureStore}; // double check consequence of object export multiple
