df0e7b58de7f236ae4ae9719f281c7c202f1daec
CShorter3 <CalidShorter3@gmail.com>
2024-10-30 18:49:32 -0400

    Adds REMOVE_USER aciton creator

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -1,6 +1,7 @@
 import { csrfFetch } from './csrf';
 
 const SET_USER = "session/setUser";
+const REMOVE_USER = "session/removeUser";
 
 // action sets session slice of state of user to action creator's input parameter
 const setUser = user => {
@@ -10,5 +11,11 @@ const setUser = user => {
     };
 };
 
-
 // action creates action to have reducer remove the session user
+const removeUser = user => {
+    return {
+        type: REMOVE_USER,
+    }
+}
+
+// create thunk action -> makes POST api/session request
