7801761ffad8246bb4ed4814fdbf65ae2125d5a4
CShorter3 <CalidShorter3@gmail.com>
2024-10-30 17:39:58 -0400

    uncomment api/test backend route to test csrfFetch method in frontend

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -22,7 +22,7 @@ router.use('/spots', require('./spots.js'));
 
 router.use('/spot-images', require('./spot-images.js'));
   
-/*const { User } = require('../../db/models');
+const { User } = require('../../db/models');
 const { setTokenCookie, requireAuth } = require('../../utils/auth.js');
 
 router.post('/test', function(req, res) {
@@ -30,7 +30,8 @@ router.post('/test', function(req, res) {
   console.log(req.body)
   res.json({ requestBody: req.body });
 });
-  
+ 
+/*
 router.get('/set-token-cookie', async (_req, res) => {
   const user = await User.findOne({
     where: {
