b3f8beef4603d48d0ec567694ffe5a03d91b558f
CShorter3 <CalidShorter3@gmail.com>
2024-10-31 18:05:48 -0400

    Add line importing all exports, into one object, from the session file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,8 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { configureStore } from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+// import all exports from session file into one object
+import * as sessionActions from './store/session';
 
 // initialize Redux store 
 const store = configureStore();
