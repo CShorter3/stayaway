ba8d6483d4b4fa3a32112251121b53286a7402c6
CShorter3 <CalidShorter3@gmail.com>
2025-01-13 00:58:56 -0500

    Adds third OpenModalButton example that passed onModalClose callback as funtion prop

--- frontend/src/components/OpenModalButton/OpenModalButton.jsx
+++ frontend/src/components/OpenModalButton/OpenModalButton.jsx
@@ -41,4 +41,16 @@ const Greeting = () => {
   );
 };
 
+
+const Greeting = () => {
+  return (
+    <OpenModalButton
+      buttonText="Greeting"
+      modalComponent={<h2>Hello World!</h2>}
+      onButtonClick={() => console.log("Greeting initiated")}
+      onModalClose={() => console.log("Greeting completed")}
+    />
+  );
+};
+
 */
