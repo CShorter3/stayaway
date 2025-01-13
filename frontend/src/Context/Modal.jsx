4159fff4e0189fa0ff22f1b440590e8e6c8d2283
CShorter3 <CalidShorter3@gmail.com>
2025-01-13 00:10:51 -0500

    Adds closeModal function

--- frontend/src/Context/Modal.jsx
+++ frontend/src/Context/Modal.jsx
@@ -10,11 +10,21 @@ export function ModalProvider({ children }) {
     const [modalContent, setModalContent] = useState(null);
     const [onModalClose, setOnModalClose] = useState(null);
   
+    // runs when Modal is unmounted
+    const closeModal = () => {
+        setModalContent(null); // clear modal contents
+        if(typeof onModalClose === "function"){
+            setOnModalClose(null);
+            onModalClose();
+        }
+    };
+
     const contextValue = {
       modalRef, // reference to modal div
       modalContent, // React component to render inside modal
       setModalContent, // function to set the React component to render inside modal
-      setOnModalClose // function to set the callback function to be called when modal is closing
+      setOnModalClose, // function to set the callback function to be called when modal is closing
+      closeModal       
     };
   
     return (
