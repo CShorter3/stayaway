d76bb15c6d10b2f390a5040bb637ec10c4d8e3ad
CShorter3 <CalidShorter3@gmail.com>
2025-01-13 00:57:08 -0500

    Adds second example using OpenModalButton that prints to console on event

--- frontend/src/components/OpenModalButton/OpenModalButton.jsx
+++ frontend/src/components/OpenModalButton/OpenModalButton.jsx
@@ -30,4 +30,15 @@ const Greeting = () => {
   );
 };
 
+
+const Greeting = () => {
+  return (
+    <OpenModalButton
+      buttonText="Greeting"
+      modalComponent={<h2>Hello World!</h2>}
+      onButtonClick={() => console.log("Greeting initiated")}
+    />
+  );
+};
+
 */
