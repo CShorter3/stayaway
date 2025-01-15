c6cd449dec24ff236ddfa237c20ea5d2054671c8
CShorter3 <CalidShorter3@gmail.com>
2025-01-15 01:34:52 -0500

    Adds tile display notes

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -1,4 +1,5 @@
 import { useDispatch } from 'react-redux';
+import { Tile } from './Tile';
 
 // HomePage will dynamically populate the grid with spot tiles
 function HomePage(){
--- frontend/src/components/HomePage/Tile.jsx
+++ frontend/src/components/HomePage/Tile.jsx
@@ -1,16 +1,26 @@
+import "./Tile.css";
+
 const Tile = ({ spot }) => {
     
+    // will handle click on tile navigates to spot page
     
     render (
+        /* display child elements vertically*/
         <div className="tile-container">
-            <div className="tile-image"> image </div>
-            <div> /* structure details row 1*/
-                <p> city, state left </p>
-                <p> spot rating right </p>
-            </div> 
+            /* tile image should take atleast 80% the tile container */
+            <div className="tile-image"> <img src={spot.imageUrl} alt="spot image" /></div>
+            // spot detail rows should take ~10% container height
+            <div className="tile-detail-row">
+                <p id="push-detail-left"> city, state left </p>
+                <p id="push-detail-right"> spot rating right </p>
+            </div>
+            <div className="tile-detail-row">
             <div> /* structure details row 2*/
-                <p> price per night left </p>
+                <p id="push-detail-left"> price per night left </p>
             </div> 
+            </div>
         </div>
     )
 }
+
+export default Tile;
