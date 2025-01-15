bdfebf40994df9a45af4142d829d8ec88ac95805
CShorter3 <CalidShorter3@gmail.com>
2025-01-15 01:40:17 -0500

    Adds spot details to a spot tile

--- frontend/src/components/HomePage/Tile.jsx
+++ frontend/src/components/HomePage/Tile.jsx
@@ -11,12 +11,12 @@ const Tile = ({ spot }) => {
             <div className="tile-image"> <img src={spot.imageUrl} alt="spot image" /></div>
             // spot detail rows should take ~10% container height
             <div className="tile-detail-row">
-                <p id="push-detail-left"> city, state left </p>
-                <p id="push-detail-right"> spot rating right </p>
+                <p id="push-detail-left"> {spot.city}, {spot.state} </p>
+                <p id="push-detail-right"> {spot.avgRating ? spot.avgRating.toFixed(1) : "New"} </p>
             </div>
             <div className="tile-detail-row">
             <div> /* structure details row 2*/
-                <p id="push-detail-left"> price per night left </p>
+                <p id="push-detail-left"> ${spot.price} <span>night</span> </p>
             </div> 
             </div>
         </div>
