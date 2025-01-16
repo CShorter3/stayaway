e15b3102cf031715e87ebee35dcd8bda8430d263
CShorter3 <CalidShorter3@gmail.com>
2025-01-16 18:34:36 -0500

    fixes data access bug, hide console logs

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -9,9 +9,9 @@ function HomePage(){
     const dispatch = useDispatch();
     // access normalized spots object from store
     const displaySpots = useSelector((state) => {
-        console.log("Redux state:", state);                 // Check the full Redux state structure
-        console.log("Redux state.spots: ", state.spots);
-        console.log("Redux state.spots.spots: ", state.spots.spots)
+        // console.log("Redux state:", state);                 // Check the full Redux state structure
+        // console.log("Redux state.spots: ", state.spots);
+        // console.log("Redux state.spots.spots: ", state.spots.spots)
         return Object.values(state.spots.spots);
     });
 
@@ -29,9 +29,9 @@ function HomePage(){
         <div className="tiles-grid">
 
             {displaySpots.map((spot) => {
-                console.log("iterating over...", displaySpots);       // expect to see the same spots object with unqiue keys at each map pass
-                console.log("current spot id: ", spot.id)   // expect spot id to be a new spot id at each map pass, which is used to key into a spot object
-                console.log("current spot object: ", spot)   // expect spot to be a new spot object that maps to a unique spots object key
+                // console.log("iterating over...", displaySpots);       // expect to see the same spots object with unqiue keys at each map pass
+                // console.log("current spot id: ", spot.id)   // expect spot id to be a new spot id at each map pass, which is used to key into a spot object
+                // console.log("current spot object: ", spot)   // expect spot to be a new spot object that maps to a unique spots object key
                 return <Tile key={spot.id} spot={spot} />
             })}
         </div>
