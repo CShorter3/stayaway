587985b29a0e288078d05b87ef56ec67b1c7f7da
CShorter3 <CalidShorter3@gmail.com>
2025-01-18 15:54:55 -0500

    Adds thunk to fetch all reviews by spot id

--- frontend/src/store/reviews.js
+++ frontend/src/store/reviews.js
@@ -1,17 +1,58 @@
 import { csrfFetch } from "./csrf";
 
-// load spot's review list for detail page
+// load spot's reviews
 export const LOAD_REVIEWS = "reviews/LOAD_REVIEWS";
 
 // posting or editing single review
 // export const LOAD_SPOT_REVIEW = "reviews/LOAD_REVIEW";
 
-// load all spotId's reviews
-export const loadSpotReviews = (reviews) => ({
+export const loadReviews = (reviews) => ({
     type: LOAD_REVIEWS,
     payload: reviews
 });
 
+export const fetchReviewsBySpotId = (spotId) => async (dispatch) => {
+    const response = await csrfFetch(`/api/spots/${spotId}/reviews`);
+
+    if(response.ok){
+        const data = await response.json();
+
+        // Normalize entire reviews object
+        const normalizedReviews = {};
+        const normalizedUsers = {};
+        const normalizedReviewImages = {};
+        
+        data.Reviews.forEach((review) => {
+            
+            // Enable normalization by flattening nested objects and including
+            // foreign keys that link to all slices of Reviews state.
+            normalizedReviews[review.id] = {
+                ...review,
+                userId: review.User.id,             // flattened, foreign key
+                spotId: review.Spot?.id || spotId   // created foregin key
+            };
+            
+            // Normalize flattened User Object - creator of review
+            normalizedUsers[review.User.id] = review.User;
+            
+            // Normalize flattened ReviewImages Object
+            review.ReviewImages.forEach((image) => {
+                normalizedReviewImages[image.id] = image;
+            });
+
+        });
+
+        dispatch(
+            loadReviews({
+                reviews: normalizedReviews,
+                users: normalizedUsers,
+                reviewImages: normalizedReviewImages
+            })
+        );
+    }
+}
+
+// **** Thunks ****
 const initialState = { 
     reviews: {},
     users: {},
@@ -25,9 +66,11 @@ const reviewsReducer = (state = initialState, action) => {
                 reviews: { ...state.reviews, ...action.payload.reviews },
                 users: { ...state.users, ...action.payload.users },
                 reviewImages: { ...state.reviewImages, ...action.payload.reviewImages }
-            };
-        };
+            }
+        }
         default:
             return state;
     }
 }
+
+export default reviewsReducer;
