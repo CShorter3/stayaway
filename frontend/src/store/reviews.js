5b1c2940d04d3c82608744793c817360c9f01cc1
CShorter3 <CalidShorter3@gmail.com>
2025-01-18 12:47:18 -0500

    Adds reducer logic

--- frontend/src/store/reviews.js
+++ frontend/src/store/reviews.js
@@ -1,8 +1,33 @@
 import { csrfFetch } from "./csrf";
 
+// load spot's review list for detail page
 export const LOAD_REVIEWS = "reviews/LOAD_REVIEWS";
 
-export const loadReviews = (spotId, reviews) => ({
+// posting or editing single review
+// export const LOAD_SPOT_REVIEW = "reviews/LOAD_REVIEW";
+
+// load all spotId's reviews
+export const loadSpotReviews = (reviews) => ({
     type: LOAD_REVIEWS,
-    payload: { spotId, reviews }
+    payload: reviews
 });
+
+const initialState = { 
+    reviews: {},
+    users: {},
+    reviewImages: {} 
+};
+
+const reviewsReducer = (state = initialState, action) => {
+    switch (action.type){
+        case LOAD_REVIEWS: {
+            return { ...state,
+                reviews: { ...state.reviews, ...action.payload.reviews },
+                users: { ...state.users, ...action.payload.users },
+                reviewImages: { ...state.reviewImages, ...action.payload.reviewImages }
+            };
+        };
+        default:
+            return state;
+    }
+}
