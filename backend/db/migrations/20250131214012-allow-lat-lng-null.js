21d9194d36aa02ee780ab9cf86697a7f440790da
CShorter3 <CalidShorter3@gmail.com>
2025-01-31 17:26:24 -0500

    troubleshoot

--- backend/db/migrations/20250131214012-allow-lat-lng-null.js
+++ backend/db/migrations/20250131214012-allow-lat-lng-null.js
@@ -1,38 +1,87 @@
 'use strict';
 
+let options = {};
+
+if(process.env.NODE_ENV === 'production'){
+  if(!process.env.SCHEMA){
+    throw new Error('SCHEMA environment variable not defined');
+  }
+  options.schema = process.env.SCHEMA;
+}
+
 /** @type {import('sequelize-cli').Migration} */
+//module.exports = {
+//   async up (queryInterface, Sequelize) {
+//     /**
+//      * Add altering commands here.
+//      *
+//      * Example:
+//      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+//      */
+//     await queryInterface.changeColumn('Spots', 'lat', {
+//       type: Sequelize.DECIMAL,
+//       allowNull: true,
+//     });
+//     await queryInterface.changeColumn('Spots', 'lng', {
+//       type: Sequelize.DECIMAL,
+//       allowNull: true, 
+//     });
+//   },
+
+//   async down (queryInterface, Sequelize) {
+//     /**
+//      * Add reverting commands here.
+//      *
+//      * Example:
+//      * await queryInterface.dropTable('users');
+//      */
+//     await queryInterface.changeColumn('Spots', 'lat', {
+//       type: Sequelize.DECIMAL,
+//       allowNull: false,
+//     });
+//     await queryInterface.changeColumn('Spots', 'lng', {
+//       type: Sequelize.DECIMAL,
+//       allowNull: false, 
+//     });
+//   }
+// };
+
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-    await queryInterface.changeColumn('Spots', 'lat', {
-      type: Sequelize.DECIMAL,
-      allowNull: true,
-    });
-    await queryInterface.changeColumn('Spots', 'lng', {
-      type: Sequelize.DECIMAL,
-      allowNull: true, 
-    });
+    await queryInterface.changeColumn(
+      { tableName: 'Spots', schema: options.schema },
+      'lat',
+      {
+        type: Sequelize.DECIMAL,
+        allowNull: true,
+      }
+    );
+    await queryInterface.changeColumn(
+      { tableName: 'Spots', schema: options.schema },
+      'lng',
+      {
+        type: Sequelize.DECIMAL,
+        allowNull: true,
+      }
+    );
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-    await queryInterface.changeColumn('Spots', 'lat', {
-      type: Sequelize.DECIMAL,
-      allowNull: false,
-    });
-    await queryInterface.changeColumn('Spots', 'lng', {
-      type: Sequelize.DECIMAL,
-      allowNull: false, 
-    });
+    await queryInterface.changeColumn(
+      { tableName: 'Spots', schema: options.schema },
+      'lat',
+      {
+        type: Sequelize.DECIMAL,
+        allowNull: false, 
+      }
+    );
+    await queryInterface.changeColumn(
+      { tableName: 'Spots', schema: options.schema },
+      'lng',
+      {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+      }
+    );
   }
-};
+}
