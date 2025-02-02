088bc32a0a72941a2c309df74d60db2129fe0efd
CShorter3 <CalidShorter3@gmail.com>
2025-02-02 15:05:17 -0500

    comment out seed data

--- backend/db/seeders/20240919231858-user.js
+++ backend/db/seeders/20240919231858-user.js
@@ -1,70 +1,84 @@
-'use strict';
+// 'use strict';
 
-const bcrypt = require('bcryptjs');
-const { User } = require('../models');
+// const bcrypt = require('bcryptjs');
+// const { User } = require('../models');
 
-const { Op } = require('sequelize');
+// const { Op } = require('sequelize');
 
-const userSeedData = [
-  {
-    firstName: 'Demo',
-    lastName: 'Lition',
-    email: 'demo@user.io',
-    username: 'Demo-lition',
-    hashedPassword: bcrypt.hashSync('password'),
-  },
-  {
-    firstName: 'Fake',
-    lastName: 'User1',
-    email: 'user1@user.io',
-    username: 'FakeUser1',
-    hashedPassword: bcrypt.hashSync('password2'),
-  },
-  {
-    firstName: 'Fake',
-    lastName: 'User2',
-    email: 'user2@user.io',
-    username: 'FakeUser2',
-    hashedPassword: bcrypt.hashSync('password3'),
-  },
-];
+// const userSeedData = [
+//   {
+//     firstName: 'Demo',
+//     lastName: 'Lition',
+//     email: 'demo@user.io',
+//     username: 'Demo-lition',
+//     hashedPassword: bcrypt.hashSync('password'),
+//   },
+//   {
+//     firstName: 'Karl',
+//     lastName: 'Malone',
+//     email: 'user1@gmail.com',
+//     username: 'KMalone',
+//     hashedPassword: bcrypt.hashSync('password'),
+//   },
+//   {
+//     firstName: 'Larry',
+//     lastName: 'Bird',
+//     email: 'user2@gmail.com',
+//     username: 'LBird',
+//     hashedPassword: bcrypt.hashSync('password'),
+//   },
+//   {
+//     firstName: 'Antwan',
+//     lastName: 'Randle El',
+//     email: 'user4@gmail.com',
+//     username: 'Aelman',
+//     hashedPassword: bcrypt.hashSync('password'),
+//   },
+//   {
+//     firstName: 'Jayden',
+//     lastName: 'Daniels',
+//     email: 'user5@gmail.com',
+//     username: 'TheKid',
+//     hashedPassword: bcrypt.hashSync('password'),
+//   }
+// ];
 
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-    await User.bulkCreate(userSeedData, { validate: true });
-  },
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   async up (queryInterface, Sequelize) {
+//     /**
+//      * Add seed commands here.
+//      *
+//      * Example:
+//      * await queryInterface.bulkInsert('People', [{
+//      *   name: 'John Doe',
+//      *   isBetaMember: false
+//      * }], {});
+//     */
+//     await User.bulkCreate(userSeedData, { validate: true });
+//   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+//   async down (queryInterface, Sequelize) {
+//     /**
+//      * Add commands to revert seed here.
+//      *
+//      * Example:
+//      * await queryInterface.bulkDelete('People', null, {});
+//      */
 
-    // like 40 mins were spent here trying to make this
-    // a cool self-building scalable query but sequelize
-    // is really just terrible in pretty much every
-    // conceivable way so we're stuck with this
-    for (const user of userSeedData) {
-      await User.destroy({
-        where: {
-          [Op.and]: [
-            { username: user.username },
-            { email: user.email },
-          ]
-        }
-      });
-    };
-  }
-};
+//     // like 40 mins were spent here trying to make this
+//     // a cool self-building scalable query but sequelize
+//     // is really just terrible in pretty much every
+//     // conceivable way so we're stuck with this
+//     for (const user of userSeedData) {
+//       await User.destroy({
+//         where: {
+//           [Op.and]: [
+//             { username: user.username },
+//             { email: user.email },
+//           ]
+//         }
+//       });
+//     };
+//   }
+// };
--- backend/db/seeders/20240924190220-review.js
+++ backend/db/seeders/20240924190220-review.js
@@ -1,40 +1,68 @@
-'use strict';
+// 'use strict';
 
-const { Review } = require('../models');
+// const { Review } = require('../models');
 
-const seedData = [
-  {
-    spotId: 2,
-    userId: 1,
-    review: 'Lorem ipsum odor amet, consectetuer adipiscing elit. Nec egestas cras taciti aenean ligula ex turpis amet vulputate sem sollicitudin scelerisque justo pretium nibh scelerisque fermentum lectus arcu.',
-    stars: 4
-  }
-];
+// let options = {};
+// if (process.env.NODE_ENV === 'production') {
+// 	options.schema = process.env.SCHEMA;
+// }
 
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-    await Review.bulkCreate(seedData);
-  },
+// const seedData = [
+//   {
+//     spotId: 1,
+//     userId: 4,
+//     review: 'Lorem ipsum odor amet, consectetuer adipiscing elit. Nec egestas cras taciti aenean ligula ex turpis amet vulputate sem sollicitudin scelerisque justo pretium nibh scelerisque fermentum lectus arcu.',
+//     stars: 4
+//   },
+//   {
+//     spotId: 1,
+//     userId: 1,
+//     review: 'Lorem ipsum odor amet, consectetuer adipiscing elit. Nec egestas cras taciti aenean ligula ex turpis amet vulputate sem sollicitudin scelerisque justo pretium nibh scelerisque fermentum lectus arcu. ipsum odor amet, consectetuer adipiscing elit.',
+//     stars: 3
+//   },
+//   {
+//     spotId: 2,
+//     userId: 3,
+//     review: 'Lorem ipsum odor amet, consecteitudin scelerisque justo pretium nibh scelerisque fermentum lectus arcu.',
+//     stars: 5
+//   },
+//   {
+//     spotId: 2,
+//     userId: 3,
+//     review: 'Lorem ipsum odor amet, consectetuer adipiscing elit. Nec egestas cras taciti aenean ligula ex turpis amet vulputate sem sollicitudin sceleri justo pretium nibh scelerisque fermentum lectus arcu.',
+//     stars: 4
+//   },
+// ];
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-    for (const review of seedData) {
-      await Review.destroy({ where: review });
-    }
-  }
-};
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   async up (queryInterface, Sequelize) {
+//     /**
+//      * Add seed commands here.
+//      *
+//      * Example:
+//      * await queryInterface.bulkInsert('People', [{
+//      *   name: 'John Doe',
+//      *   isBetaMember: false
+//      * }], {});
+//     */
+//     await Review.bulkCreate(seedData, {validate: true});
+//   },
+
+//   async down (queryInterface, Sequelize) {
+//     /**
+//      * Add commands to revert seed here.
+//      *
+//      * Example:
+//      * await queryInterface.bulkDelete('People', null, {});
+//      */
+//   options.tableName = 'Reviews';
+//   const Op = Sequelize.Op;
+//   return queryInterface.bulkDelete(
+//     options,
+//     review: { [Op.in]: seedData.map(image => image.url) },
+
+//   )
+
+//   }
+// };
